ffdad0780453310828e3cbf294140a6fa831dbcb
maxime_98 <maximengnama@gmail.com>
2023-01-12 09:43:28 +0100

    Création des components

---
+++ src/components/RestaurantRow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Restaurant--Row">
+    <h2 class="title">
+        Nos restaurants
+    </h2>
+    <restaurant-card/>
+    </div>
+</template>
+
+<script>
+import RestaurantCard from './RestaurantCard.vue';
+
+export default {
+    name:"RestaurantRow",
+    components: {
+        RestaurantCard,
+    }
+}
+</script>
+
+<style>
+
+</style>
